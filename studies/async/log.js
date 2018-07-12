a48f7e7f48b9fd2228679abf4d20dc0cd23099ee
jblflqr <jean-baptiste.laflaquiere@protonmail.com>
2018-07-12 10:04:11 +0200

    mise en place progress bar

--- studies/async/log.js
+++ studies/async/log.js
@@ -1,17 +1,16 @@
-const log = (content) => {
-    let p = document.createElement("p");
+var log = function (content) {
+    var p = document.createElement("p");
     p.innerHTML = content;
     document.body.appendChild(p);
 };
-
-const Logger = {
+var Logger = {
     container: 'div[id=log]',
-    log: content => {
-        let el = document.createElement("div");
+    log: function (content) {
+        var el = document.createElement("div");
         el.innerHTML = content;
         document.querySelector(Logger.container).appendChild(el);
     },
-    clear: () => {
+    clear: function () {
         document.querySelector(Logger.container).innerHTML = '';
     }
-}
+};
--- studies/async/mozilla-example.ts
+++ studies/async/mozilla-example.ts
@@ -1,58 +1,55 @@
-var resolveAfter2Seconds = function() {
-  log("starting slow promise");
+let startSlowProcess = () => fillProgressBar(slowProgressBar(), 10);
+let startFastProcess = () => fillProgressBar(fastProgressBar(), 20);
+
+let fillProgressBar = (bar, incr) => {
+  Logger.log(`Promise(${incr}) started...`);
   return new Promise(resolve => {
-    setTimeout(function() {
-      resolve(20);
-      log("slow promise is done");
-    }, 2000);
+    let progress = setInterval(() => {
+      bar.value += incr;
+      if (bar.value >= bar.max) {
+        clearInterval(progress);
+        resolve(incr)
+        Logger.log(`Promise(${incr}) done !`);
+      }
+    }, 50)
   });
 };
 
-var resolveAfter1Second = function() {
-  log("starting fast promise");
-  return new Promise(resolve => {
-    setTimeout(function() {
-      resolve(10);
-      log("fast promise is done");
-    }, 1000);
-  });
-};
+let fastProgressBar = () => document.querySelector('progress[name=fast]');
+let slowProgressBar = () => document.querySelector('progress[name=slow]');
 
-var sequentialStart = async function() {
-  log('==SEQUENTIAL START==');
-  const slow = await resolveAfter2Seconds(); // If the value of the expression following the await operator is not a Promise, it's converted to a resolved Promise.
-  const fast = await resolveAfter1Second();
-  log(slow);
-  log(fast);
+let resetAll = () => {
+  Logger.clear();
+  fastProgressBar().value = 0;
+  slowProgressBar().value = 0;
 }
 
-var concurrentStart = async function() {
-  log('==CONCURRENT START with await==');
-  const slow = resolveAfter2Seconds(); // starts timer immediately
-  const fast = resolveAfter1Second();
-
-  log(await slow);
-  log(await fast); // waits for slow to finish, even though fast is already done!
+var sequentialStart = async function () {
+  resetAll();
+  const slow = await startSlowProcess(); // If the value of the expression following the await operator is not a Promise, it's converted to a resolved Promise.
+  const fast = await startFastProcess();
+  Logger.log(slow);
+  Logger.log(fast);
 }
 
-var stillSerial = function() {
-  log('==CONCURRENT START with Promise.all==');
-  Promise.all([resolveAfter2Seconds(), resolveAfter1Second()]).then(([slow, fast]) => {
-    log(slow);
-    log(fast);
+var concurrentStart = async function () {
+  resetAll();
+  const slow = startSlowProcess(); // starts timer immediately
+  const fast = startFastProcess();
+  Logger.log(await slow);
+  Logger.log(await fast); // waits for slow to finish, even though fast is already done!
+}
+
+var promiseAll = function () {
+  resetAll();
+  Promise.all([startSlowProcess(), startFastProcess()]).then(([slow, fast]) => {
+    Logger.log(slow);
+    Logger.log(fast);
   });
 }
 
-var parallel = function() {
-  log('==PARALLEL with Promise.then==');
-  resolveAfter2Seconds().then((message)=>log(message)); // in this case could be simply written as log(resolveAfter2Seconds());
-  resolveAfter1Second().then((message)=>log(message));
+var parallel = function () {
+  resetAll();
+  startSlowProcess().then((message) => Logger.log(message)); // in this case could be simply written as log(startSlowProcess());
+  startFastProcess().then((message) => Logger.log(message));
 }
-
-sequentialStart(); // takes 2+1 seconds in total
-// wait above to finish
-setTimeout(concurrentStart, 4000); // takes 2 seconds in total
-// wait again
-setTimeout(stillSerial, 7000); // same as before
-// wait again
-setTimeout(parallel, 10000); // trully parallel
