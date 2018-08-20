43deb5fe41fc614ae0e8ec0a32c68602cbe65eb5
jblflqr <jean-baptiste.laflaquiere@protonmail.com>
2018-08-20 23:32:30 +0200

    wip

--- studies/rxjs/test1.js
+++ studies/rxjs/test1.js
@@ -1,5 +1,30 @@
-var Observable = rxjs.Observable, Subject = rxjs.Subject, ReplaySubject = rxjs.ReplaySubject, from = rxjs.from, of = rxjs.of, range = rxjs.range;
-var _a = rxjs.operators, map = _a.map, filter = _a.filter, switchMap = _a.switchMap;
-range(1, 200)
-    .pipe(filter(function (x) { return x % 2 === 1; }), map(function (x) { return x + x; }))
-    .subscribe(function (x) { return console.log(x); });
+var Observable = rxjs.Observable, Subject = rxjs.Subject, ReplaySubject = rxjs.ReplaySubject, from = rxjs.from, of = rxjs.of, range = rxjs.range, fromEvent = rxjs.fromEvent;
+var _a = rxjs.operators, map = _a.map, filter = _a.filter, switchMap = _a.switchMap, scan = _a.scan;
+var startSlowProcess = function () { return fillProgressBar('Slow process', slowProgressBar(), 100); };
+var startFastProcess = function () { return fillProgressBar('Fast process', fastProgressBar(), 50); };
+var fastProgressBar = function () { return document.querySelector('progress[name=fast]'); };
+var slowProgressBar = function () { return document.querySelector('progress[name=slow]'); };
+var logger = new Logger().setContainerSelector('div[id=log]');
+var fillProgressBar = function (name, bar, interval) {
+    logger.log(name + " started...");
+    return new Promise(function (resolve) {
+        var begin = new Date().valueOf();
+        var progress = setInterval(function () {
+            bar.value += 5;
+            if (bar.value >= bar.max) {
+                clearInterval(progress);
+                logger.log(name + " done !");
+                resolve("[" + name + " resolved] Execution time : " + (new Date().valueOf() - begin) + " ms");
+            }
+        }, interval);
+    });
+};
+var resetAll = function () {
+    logger.clear();
+    fastProgressBar().value = 0;
+    slowProgressBar().value = 0;
+};
+range(1, 10)
+    .pipe(filter(function (x) { return x % 2 === 1; }), map(function (x) { return x + x; })).subscribe(function (x) { return console.log(x); });
+var scanButton = document.querySelector('#scanButton');
+fromEvent(scanButton, 'click').pipe(scan(function (count) { return count + 1; }, 0)).subscribe(function (count) { return logger.log("Clicked " + count + " times"); });
--- studies/rxjs/test1.ts
+++ studies/rxjs/test1.ts
@@ -1,6 +1,45 @@
-const { Observable, Subject, ReplaySubject, from, of, range } = rxjs;
-const { map, filter, switchMap } = rxjs.operators;
+const {Observable, Subject, ReplaySubject, from, of, range, fromEvent} = rxjs;
+const {map, filter, switchMap, scan} = rxjs.operators;
 
-range(1, 200)
-  .pipe(filter(x => x % 2 === 1), map(x => x + x))
-  .subscribe(x => console.log(x));
+
+let startSlowProcess = () => fillProgressBar('Slow process', slowProgressBar(), 100);
+let startFastProcess = () => fillProgressBar('Fast process', fastProgressBar(), 50);
+
+let fastProgressBar = () => document.querySelector('progress[name=fast]');
+let slowProgressBar = () => document.querySelector('progress[name=slow]');
+
+var logger = new Logger().setContainerSelector('div[id=log]');
+
+let fillProgressBar = (name, bar, interval) => {
+  logger.log(`${name} started...`);
+  return new Promise(resolve => {
+    let begin = new Date().valueOf();
+    let progress = setInterval(() => {
+      bar.value += 5;
+      if (bar.value >= bar.max) {
+        clearInterval(progress);
+        logger.log(`${name} done !`);
+        resolve(`[${name} resolved] Execution time : ${new Date().valueOf() - begin} ms`);
+      }
+    }, interval)
+  });
+};
+
+let resetAll = () => {
+  logger.clear();
+  fastProgressBar().value = 0;
+  slowProgressBar().value = 0;
+}
+
+range(1, 10)
+  .pipe(
+    filter(x => x % 2 === 1),
+    map(x => x + x)
+  ).subscribe(x => console.log(x));
+
+
+const scanButton = document.querySelector('#scanButton');
+
+fromEvent(scanButton, 'click').pipe(
+  scan(count => count + 1, 0)
+).subscribe(count => logger.log(`Clicked ${count} times`));
